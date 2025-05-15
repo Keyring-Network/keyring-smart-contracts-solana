b1d52713dd96875a0fe695ca910c3d6231d0568c
Sahil-Gujrati <sahilgujrati12@gmail.com>
2025-05-15 23:45:22 +0530

    fix: Use correct seeds

--- scripts/utils/getPda.ts
+++ scripts/utils/getPda.ts
@@ -2,7 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 
 const getProgramStatePda = (programId: anchor.web3.PublicKey) =>
     anchor.web3.PublicKey.findProgramAddressSync(
-        [Buffer.from("keyring_network"), Buffer.from("global_state")],
+        [Buffer.from("keyring_program"), Buffer.from("global_state")],
         programId
     )[0];
 
